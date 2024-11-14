f94957735e4bf83786f46f78a0d61459a92fb88f
Jake Bown <jakebown@gmail.com>
2024-11-14 04:50:47 +0000

    Added ACF Fields and modifiers

--- template.php
+++ template.php
@@ -24,9 +24,29 @@
         'classes'       => ['{slug}', 'no-js']
     ));
 
+    /*
+     * Register ACF Fields
+     */ 
+    $block->register_fields(array(
+        array(
+            'label' => 'Heading',
+            'name' => 'heading',
+            'type' => 'text'
+        )
+    ));
+
+    /*
+     * Add Field Modifiers
+     * Change the element before it's rendered.
+     */
+    $block->add_modifier('heading', function( $string ) {
+        return "<em>{$string}</em>";
+    });
+
 ?>
 <section class="<?php echo $block->classes(); ?>" id="<?php echo $block->id(); ?>"  data-tracking="<?php echo $block->id(); ?>" data-block-slug="<?php echo $block->namespace(); ?>" data-block-version="<?php echo $block->version(); ?>">
     <div class="placeholder">
+        <h1><?php echo $block->heading(); ?></h1>
         <h2>I'm a {name} Block!</h2>
     </div>
 </section>
